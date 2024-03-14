3f1675ec75580c99542310a2b3584e107fc7115e
EveOrtiz06 <141963685+EveOrtiz06@users.noreply.github.com>
2024-03-13 19:41:05 -0500

    mi
    cambio prueba

---
+++ script.js
@@ -0,0 +1,17 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "Evelyn";
+//nombre = "Evelyn";
+//console.log(nombre);
+ 
+//Condicionales
+let contenidoTitulo = "Nombre";
+
+//let titulo = document.querySelector (".logo .fuente");
+//titulo.innerHTML = contenidoTitulo;
+
+if (textoTitulo == "Nombre") {
+    titulo.innerHTML = "Otro";
+}  else {
+    console.log("no se cumple");
+}
